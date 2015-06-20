da5a7d53ef7f9e52e58dd845478261fafded395d
Clint Goodman <ClintGoodman27@Gmail.com>
2015-06-20 13:10:02 -0600

    Adding my first println

--- LightsOut/src/lightsout/LightsOut.java
+++ LightsOut/src/lightsout/LightsOut.java
@@ -15,7 +15,8 @@ public class LightsOut {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //This is my new code
+        System.out.println("HI");
     }
     
 }
