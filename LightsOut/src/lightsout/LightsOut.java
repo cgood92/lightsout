e2dd4fc82921aced2c4bdedc2cc40286883c9149
Tyler Barton <TylerBarton@Gmail.com>
2015-06-20 23:54:08 -0600

    taking out unneeded code

--- LightsOut/src/lightsout/LightsOut.java
+++ LightsOut/src/lightsout/LightsOut.java
@@ -27,15 +27,6 @@ public class LightsOut {
                 }
             });
         }
-        catch(Throwable ex){
-            System.out.println("There was an unexpected error in the game.\nProgram will now close.");
-            System.out.println(ex.getStackTrace());
-        }
-        finally{
-            if (LightsOut.mainFrame != null) {
-                LightsOut.mainFrame.dispose();
-            }
-        }
     }
 }
 
